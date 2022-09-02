0daf052b71e825379fb86b1bb8e7942a786b0329
NicoStuer <110864292+NicoStuer@users.noreply.github.com>
2022-09-02 13:10:18 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>`;
+
+//let a:number =2  
+//let b:number =6
+//console.log(a+b);
+ 
+let playLol:string = `Crear una cuenta "Riot"`
+let playLol2:string = "Ingresar datos personales"
+let playLol3:string = `Generar un "Username"`
+let playLol4:string = `Generar un "Pasword`
+let playLol5:string = "Autentificar cuenta mediante mail"
+let playLol6:string = "Ingresarl al juego"
+let playLol7:string = "Pasala lindo"
+
+console.log(playLol+ "\n"+ playLol2+ "\n"+ playLol3+ "\n"+ playLol4+ "\n"+ playLol5+ "\n"+ playLol6+ "\n"+ playLol7);
+
+
+
